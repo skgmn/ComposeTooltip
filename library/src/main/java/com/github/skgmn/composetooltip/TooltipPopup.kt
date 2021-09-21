10113eba531a48472134302f70ad4f5a9ab6a63b
skgmn <suckgamony@gmail.com>
2021-09-21 11:21:38 +0900

    support transition for tooltip popup

--- library/src/main/java/com/github/skgmn/composetooltip/TooltipPopup.kt
+++ library/src/main/java/com/github/skgmn/composetooltip/TooltipPopup.kt
@@ -1,14 +1,25 @@
 package com.github.skgmn.composetooltip
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupPositionProvider
 import androidx.compose.ui.window.PopupProperties
+import kotlinx.coroutines.delay
+
+private const val TRANSITION_INITIALIZE = 0
+private const val TRANSITION_ENTER = 1
+private const val TRANSITION_EXIT = 2
+private const val TRANSITION_GONE = 3
 
 @Composable
 fun Tooltip(
@@ -33,19 +44,120 @@ fun Tooltip(
         onDismissRequest = onDismissRequest,
         properties = properties
     ) {
-        TooltipContainer(
-            modifier = Modifier,
-            cornerRadius = tooltipStyle.cornerRadius,
+        TooltipImpl(
+            tooltipStyle = tooltipStyle,
             tipPosition = tipPosition,
-            tip = { Tip(anchorEdge, tooltipStyle) },
-            content = {
-                TooltipContentContainer(
-                    anchorEdge = anchorEdge,
+            anchorEdge = anchorEdge,
+            content = content
+        )
+    }
+}
+
+@Composable
+private fun AnchorEdge.TooltipImpl(
+    tooltipStyle: TooltipStyle,
+    tipPosition: EdgePosition,
+    anchorEdge: AnchorEdge,
+    modifier: Modifier = Modifier,
+    content: @Composable() (RowScope.() -> Unit)
+) {
+    TooltipContainer(
+        modifier = modifier,
+        cornerRadius = tooltipStyle.cornerRadius,
+        tipPosition = tipPosition,
+        tip = { Tip(anchorEdge, tooltipStyle) },
+        content = {
+            TooltipContentContainer(
+                anchorEdge = anchorEdge,
+                tooltipStyle = tooltipStyle,
+                content = content
+            )
+        }
+    )
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun Tooltip(
+    anchorEdge: AnchorEdge,
+    enterTransition: EnterTransition,
+    exitTransition: ExitTransition,
+    visible: Boolean = true,
+    tooltipStyle: TooltipStyle = rememberTooltipStyle(),
+    tipPosition: EdgePosition = remember { EdgePosition() },
+    anchorPosition: EdgePosition = remember { EdgePosition() },
+    margin: Dp = 8.dp,
+    onDismissRequest: (() -> Unit)? = null,
+    properties: PopupProperties = remember { PopupProperties() },
+    content: @Composable RowScope.() -> Unit,
+) = with(anchorEdge) {
+    var transitionState by remember { mutableStateOf(TRANSITION_GONE) }
+    LaunchedEffect(visible) {
+        if (visible) {
+            when (transitionState) {
+                TRANSITION_EXIT -> transitionState = TRANSITION_ENTER
+                TRANSITION_GONE -> {
+                    transitionState = TRANSITION_INITIALIZE
+                    delay(1)
+                    transitionState = TRANSITION_ENTER
+                }
+            }
+        } else {
+            when (transitionState) {
+                TRANSITION_INITIALIZE -> transitionState = TRANSITION_GONE
+                TRANSITION_ENTER -> transitionState = TRANSITION_EXIT
+            }
+        }
+    }
+    if (transitionState != TRANSITION_GONE) {
+        Popup(
+            popupPositionProvider = TooltipPopupPositionProvider(
+                LocalDensity.current,
+                anchorEdge,
+                tooltipStyle,
+                tipPosition,
+                anchorPosition,
+                margin
+            ),
+            onDismissRequest = onDismissRequest,
+            properties = properties
+        ) {
+            if (transitionState == TRANSITION_INITIALIZE) {
+                TooltipImpl(
                     tooltipStyle = tooltipStyle,
+                    tipPosition = tipPosition,
+                    anchorEdge = anchorEdge,
+                    modifier = Modifier.alpha(0f),
+                    content = content,
+                )
+            }
+            AnimatedVisibility(
+                visible = transitionState == TRANSITION_ENTER,
+                enter = enterTransition,
+                exit = exitTransition
+            ) {
+                remember {
+                    object : RememberObserver {
+                        override fun onAbandoned() {
+                            transitionState = TRANSITION_GONE
+                        }
+
+                        override fun onForgotten() {
+                            transitionState = TRANSITION_GONE
+                        }
+
+                        override fun onRemembered() {
+                        }
+                    }
+                }
+                TooltipImpl(
+                    tooltipStyle = tooltipStyle,
+                    tipPosition = tipPosition,
+                    anchorEdge = anchorEdge,
                     content = content
                 )
             }
-        )
+        }
     }
 }
 
