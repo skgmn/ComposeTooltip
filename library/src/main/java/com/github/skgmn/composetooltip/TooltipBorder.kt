69194b6dd6c97b3b9d3a55074b862c07ab84c8f5
Maaruf Dauda <mkdauda.work@gmail.com>
2021-11-09 10:09:41 +0100

    Create attrb class for border

---
+++ library/src/main/java/com/github/skgmn/composetooltip/TooltipBorder.kt
@@ -0,0 +1,12 @@
+package com.github.skgmn.composetooltip
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+data class TooltipBorder  constructor(
+    val borderWidth: Dp,
+    val borderColor: Color
+    ){
+
+}
